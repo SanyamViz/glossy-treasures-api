1ff4bbb1ee2bc023d539de141489c540ed7340ed
SanyamViz <sanyamgupta6000@gmail.com>
2026-05-11 23:08:41 +0530

    fix

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/api/wholesale', wholesaleRouter);
 app.use('/api/discounts', discountRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/newsletter', newsletterRoutes);
-app.use('/api', paymentsRouter); // Maps to /api/create-order
+app.use('/api/orders', paymentsRouter); // Maps to /api/create-order
 app.use('/api/reviews', reviewRoutes);
 
 
